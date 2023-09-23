2464bafa1155efc487d58c287f8eac712b14a0eb
joaopguerram <machadojp00@gmail.com>
2023-09-23 09:08:36 -0300

    finalizacao version 1.0

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -11,16 +11,18 @@ const About = () => {
           className="imgTruck"
         />
       </div>
-      <h1>Guincho 24 horas em todo DF e Brasil</h1>
-      <p>
-        Procurando assistência confiável e rápida? Você está no lugar certo!
-        Somos uma equipe de especialistas em serviços de guinchos, auto socorro
-        e resgates dedicados a garantir que você esteja sempre seguro e em
-        movimento, não importa a situação. Com uma equipe experiente, estamos
-        prontos para atender a todas as suas necessidades de guincho, auto
-        socorro, resgate, pane seca e recarga de bateria a qualquer hora, em
-        qualquer lugar.
-      </p>
+      <div className="description">
+        <h1>Guincho 24 horas em todo DF e Brasil</h1>
+        <p>
+          Procurando assistência confiável e rápida? Você está no lugar certo!
+          Somos uma equipe de especialistas em serviços de guinchos, auto
+          socorro e resgates dedicados a garantir que você esteja sempre seguro
+          e em movimento, não importa a situação. Com uma equipe experiente,
+          estamos prontos para atender a todas as suas necessidades de guincho,
+          auto socorro, resgate, pane seca e recarga de bateria a qualquer hora,
+          em qualquer lugar.
+        </p>
+      </div>
     </main>
   );
 };
